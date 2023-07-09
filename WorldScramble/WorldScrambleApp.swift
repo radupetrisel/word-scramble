c65812c7fc8d73e9a5fad38fa18a0374ca5955fd
Radu Petrisel <radupetrisel@gmail.com>
2023-07-09 10:16:15 +0300

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Radu Petrisel on 09.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
